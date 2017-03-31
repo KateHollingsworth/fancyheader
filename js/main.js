e6abb7a11d8cde2bcd9ad1bb9ef7496dbb142856
Kate Hollingsworth <kate@drylandsalon.com>
2017-03-30 20:53:53 -0400

    Change to eventlisteners on nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/** Below works, but is oldfashioned
 function changeActive (clickedOne) {
   var links = document.getElementsByTagName('a');
   for (var i=0; i < links.length; i++) {
@@ -5,3 +6,18 @@ function changeActive (clickedOne) {
   }
     clickedOne.className = "active";
 }
+**/
+
+(function () {
+  var nav = document.querySelector('nav');
+  var links = document.querySelectorAll('li a');
+
+  nav.addEventListener('click', eventHandler, false);
+
+  function eventHandler (event) {
+    for (let i=0; i < links.length; i++) {
+      links[i].classList.remove('active');
+    }
+    event.target.classList.add('active');
+  }
+}())
